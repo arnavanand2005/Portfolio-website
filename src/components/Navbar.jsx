9eefa663dfc5b8579608bcd729ed2c04ca722ec7
arnavanand2005 <arnavanand2005@gmail.com>
2025-06-25 00:11:02 +0530

    navbar prototype finalised

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import logo from '../assets/logo.png'
 import '../styles/Navbar.css'
 
 export default function Navbar() {
   return (
     <nav className='navbar-container'>
         <div className="navbar-leftside">
-            <img src={logo} alt="Arnav's logo"/>
+            <h3 className='main-heading'>Arnav Anand</h3>
+        </div>
+        <div className="navbar-middle">
+            <p>Home</p>
+            <p>Skills</p>
+            <p>Projects</p>
         </div>
         <div className="navbar-rightside">
             <LinkedInIcon className='linkedin-icon'/>
             <GitHubIcon className='github-icon'/>
+            <button className='contact-btn'>Lets Connect</button>
         </div>
     </nav>
   )
