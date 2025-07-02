9c368ae0cf790b2d19f1330d09f6deb6fdd9ef01
arnavanand2005 <arnavanand2005@gmail.com>
2025-07-02 22:41:16 +0530

    background fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ import Home from './components/Home'
 
 export default function App() {
   return (
-    <div>
+    <>
+      <div className="fixed inset-0 -z-10 min-h-screen w-full bg-[radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div>
         <Navbar />
         <Home />
         <Contact />
-    </div>
+      </div>
+    </>
   )
 }
