7060d80dac1555fd4c0e32b85721996c8cb30068
arnavanand2005 <arnavanand2005@gmail.com>
2025-07-08 04:39:01 +0530

    framer motion added to tech-stack

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,10 +4,24 @@ import { SiExpress, SiMongodb, SiVite } from "react-icons/si";
 import { DiJavascript } from "react-icons/di";
 import { GrTechnology } from "react-icons/gr";
 import { RiTailwindCssLine } from "react-icons/ri";
+import {animate, motion } from 'framer-motion';
 
 const Skills = () => {
 
-  const iconClasses = 'text-7xl'; // Keep icons large
+  const floatVariants = (duration) => ({
+    initial: {y:0},
+    animate: {
+      y:[-10,10],
+      transition: {
+        repeat : Infinity,
+        duration: duration,
+        repeatType: 'reverse',
+        ease: 'linear'
+      },
+    },
+  });
+
+  const iconClasses = 'text-7xl'; 
 
   const iconContainerClasses = 'rounded-2xl border-4 border-neutral-800 p-4 flex items-center justify-center';
 
@@ -30,58 +44,62 @@ const Skills = () => {
                 0_0_16px_#7c3aed,   
                 0_0_24px_#22d3ee,   
                 0_0_32px_#7c3aed]'>
+
           <GrTechnology className="inline-block text-cyan-400 mr-4 text-6xl [text-shadow:0_0_8px_#22d3ee,0_0_16px_#7c3aed]" />
           My Tech-Stack
         </span>
+
       </h1>
-      <div className='flex-wrap flex items-center justify-center gap-4'>
+      <div className='flex-wrap flex items-center justify-center gap-7'>
 
-        <div className={iconContainerClasses}>
+        <motion.div  animate="animate" initial="initial" variants={floatVariants(2.5)}
+        className={iconContainerClasses}>
         <FaPython
-  className={`
-    ${iconClasses}
-    text-yellow-400 
-    [text-shadow:0_0_8px_#38BDF8]
-  `}
-/>
-          </div>
+            className={`
+              ${iconClasses}
+              text-blue-300             
+              [text-shadow:0_0_8px_#93C5FD,0_0_16px_#60A5FA] 
+            `}
+          />
+          </motion.div>
 
-        <div className={iconContainerClasses}>
+        <motion.div  animate="animate" initial="initial" variants={floatVariants(3)}
+         className={iconContainerClasses}>
           <FaReact className={`text-cyan-400 ${iconClasses}`} />
-        </div>
+        </motion.div>
 
-        <div className={iconContainerClasses}>
+        <motion.div animate="animate" variants={floatVariants(2)} initial="initial" className={iconContainerClasses}>
           <FaHtml5 className={`text-orange-600 ${iconClasses}`} />
-        </div>
+        </motion.div>
 
-        <div className={iconContainerClasses}>
+        <motion.div animate="animate" variants={floatVariants(3.5)} initial="initial" className={iconContainerClasses}>
           <FaCss3Alt className={`text-blue-500 ${iconClasses}`} />
-        </div>
+        </motion.div>
 
-        <div className={iconContainerClasses}>
+        <motion.div animate="animate" variants={floatVariants(5)} initial="initial" className={iconContainerClasses}>
           <DiJavascript className={`text-yellow-400 ${iconClasses}`} />
-        </div>
+        </motion.div>
 
-        <div className={iconContainerClasses}>
+        <motion.div animate="animate" variants={floatVariants(3.6)} initial="initial" className={iconContainerClasses}>
           <SiExpress className={`text-neutral-500 ${iconClasses}`} />
-        </div>
+        </motion.div>
 
-        <div className={iconContainerClasses}>
+        <motion.div animate="animate" variants={floatVariants(4.3)} initial="initial" className={iconContainerClasses}>
           <SiMongodb className={`text-green-500 ${iconClasses}`} />
-        </div>
+        </motion.div>
 
-        <div className={iconContainerClasses}>
+        <motion.div animate="animate" variants={floatVariants(3.8)} initial="initial" className={iconContainerClasses}>
           <SiVite className={`text-purple-500 ${iconClasses}`} />
-        </div>
+        </motion.div>
 
-        <div className={iconContainerClasses}>
+        <motion.div animate="animate" variants={floatVariants(2.4)} initial="initial" className={iconContainerClasses}>
         <RiTailwindCssLine
             className={`
               text-cyan-700       
               ${iconClasses} 
             `}
           />        
-        </div>
+        </motion.div>
 
       </div>
     </div>
